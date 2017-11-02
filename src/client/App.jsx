872596931a7da6ffd1ef9101443a09d09d614561
Mario <mario.saliba98@gmail.com>
2017-11-02 22:44:23 +0200

    setup axios

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,22 +1,78 @@
-import React from "react";
-import { render } from "react-dom";
-import styled from "styled-components";
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import Axios from 'axios';
 
-const Title = styled.h1`
-  display: block;
-  color: blue;
+const Label = styled.label`
+  margin-top: 1rem;
+  display: flex;
+  flex-direction: column;
 `;
 
-const Wrapper = styled.div`
+const NameMessage = styled.span`
+  margin-top: 1rem;
+  color: red;
+  font-size: 1rem;
+`;
+
+const Input = styled.input`
+  display: inline-block;
+`;
+const Wrapper = styled.form`
+  width: 50%;
+  margin-right: auto;
+  margin-left: auto;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 `;
 
-const App = () => (
-  <Wrapper>
-    <Title>Hello</Title>
-  </Wrapper>
-);
+const Button = styled.button`
+  margin-top: 1rem;
+`;
 
-render(<App />, document.getElementById("root"));
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      nameMessage: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const englishChar = /[a-zA-Z]/;
+    const { name, age } = event.target.elements;
+    const data = {
+      name: name.value,
+      age: age.value
+    };
+
+    if (!englishChar.test(data.name)) {
+      this.setState({ nameMessage: <NameMessage>English characters only</NameMessage> });
+    } else {
+      Axios.post('/add-data', data);
+      window.location.href = '/doSomething';
+    }
+  }
+  render() {
+    return (
+      <Wrapper onSubmit={this.handleSubmit}>
+        <Label>
+          Name:
+          <Input type="text" name="name" required />
+        </Label>
+        {this.state.nameMessage}
+        <Label>
+          Age:
+          <Input type="number" name="age" required />
+        </Label>
+        <Button type="Submit">Submit</Button>
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
