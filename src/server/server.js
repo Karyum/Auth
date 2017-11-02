335f2c776fd2e8aeadd1eef04841bb2d63e270df
Mario <mario.saliba98@gmail.com>
2017-11-02 22:44:15 +0200

    sertup body parser

--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../..', 'public')));
+app.use(bodyParser.json());
+
+app.post('/add-data', (req, res) => {
+  console.log(req.body);
+});
+
 app.get('/*', (req, res) => res.sendFile(path.resolve(__dirname, '../..', 'index.html')));
 
 app.listen(8080, () => console.log('ONN 8080'));
