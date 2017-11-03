66026f3e3b05507a5f30e310ce39aa02e383e54f
Mario <mario.saliba98@gmail.com>
2017-11-03 23:54:33 +0200

    adds a 404 page

--- src/client/404.jsx
+++ src/client/404.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const FourOhFour = () => <h1>Mate its 404 piss off</h1>;
+const Title = styled.h1`
+  margin-right: auto;
+  margin-left: auto;
+`;
+
+const FourOhFour = () => <Title>Mate its 404 piss off</Title>;
 
 export default FourOhFour;
