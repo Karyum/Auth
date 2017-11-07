2f0204694001f98e68d1198b4854e4ea84b16c6d
Mario <mario.saliba98@gmail.com>
2017-11-07 21:54:09 +0200

    add a promise library for mongoose

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const router = require('./controllers/index');
 
@@ -8,6 +9,8 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../..', 'public')));
 app.use(bodyParser.json());
+
+mongoose.Promise = require('bluebird');
 
 app.use(router);
 
