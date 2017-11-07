e6793b09d2afef8bdb5ccff9a595b5f2e70eff9b
Mario <mario.saliba98@gmail.com>
2017-11-07 21:54:22 +0200

    update mongoose schema

--- schema.js
+++ schema.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
 
 const usersSchema = mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true }
+  username: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 });
 
 const users = mongoose.model('Users', usersSchema);
