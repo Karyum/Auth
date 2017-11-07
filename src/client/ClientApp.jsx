b6a9729401092a28cd84a585b04bd76ec08c95d2
Mario <mario.saliba98@gmail.com>
2017-11-07 21:55:03 +0200

    modify routes

--- src/client/ClientApp.jsx
+++ src/client/ClientApp.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
 
 import Login from './Login';
 import FourOhFour from './404';
 import Signup from './Signup';
 
+const history = createBrowserHistory();
+
 const ClientApp = () => (
-  <BrowserRouter>
+  <Router history={history}>
     <Switch>
-      <Route exact path="/" component={Signup} />
+      <Route exact path="/signup" component={Signup} />
       <Route exact path="/login" component={Login} />
       <Route component={FourOhFour} />
     </Switch>
-  </BrowserRouter>
+  </Router>
 );
 
 render(<ClientApp />, document.getElementById('app'));
