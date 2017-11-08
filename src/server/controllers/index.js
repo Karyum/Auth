be6c76d38c4c3afa10a273800db8961627493814
Mario <mario.saliba98@gmail.com>
2017-11-08 19:19:31 +0200

    th

--- src/server/controllers/index.js
+++ src/server/controllers/index.js
@@ -3,9 +3,11 @@ const path = require('path');
 
 const signup = require('./signup.js');
 const login = require('./login.js');
+const verify = require('./verify.js');
 
 router.post('/signup', signup);
 router.post('/login', login);
+router.post('/verify', verify);
 
 router.get('/*', (req, res) => res.sendFile(path.resolve(__dirname, '../../..', 'index.html')));
 
